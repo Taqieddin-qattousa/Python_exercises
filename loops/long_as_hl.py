047dc6cfca3eb0c21b9ee4cc0b07c2b8a56ebd64
Yazan-Isam-Qa <161217218+Yazan-Isam-Qa@users.noreply.github.com>
2024-04-24 15:07:04 +0300

    Add files via upload

---
+++ loops/long_as_hl.py
@@ -0,0 +1,7 @@
+for x in range(99, 0, -1):
+  print(f' {x} bottles of beer on the wall')
+  print(' ')
+  print(f' {x} bottles of beer')
+  print(' ')
+  print(' Take one down, pass it around')
+  print(' ')
